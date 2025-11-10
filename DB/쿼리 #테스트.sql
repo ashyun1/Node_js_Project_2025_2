fb4133a27e4bc160bf80283418dd1cbd4898715b
ashyun1 <2022137060@g.shingu.ac.kr>
2025-11-10 12:04:33 +0900

    [Feat] DB -> 서버 -> 클라이언트 데이터 전달 테스트

---
+++ DB/쿼리 #테스트.sql
@@ -0,0 +1,2 @@
+SELECT q.* , pq.`STATUS` FROM player_quests pq JOIN quests q ON pq.quest_id = q.quest_id WHERE pq.player_id = 2
+SELECT inv.item_id , inv.quantity FROM inventories inv JOIN items i ON inv.item_id = i.item_id WHERE inv.player_id = 1
